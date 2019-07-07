439da6e40c739efa6373e216263feb477564bf7a
GIT SERVICE NAME (NOT USERNAME) <GIT SERVICE EMAIL>
2019-07-06 22:20:12 -0700

    contact form

--- src/components/contact/contact.js
+++ src/components/contact/contact.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import styles from './contact.css';
+import Axios from 'axios';
 
 const Contact = class Contact extends React.Component {
     constructor(props) {
@@ -7,36 +8,49 @@ const Contact = class Contact extends React.Component {
         this.state = {
             name: '',
             email: '',
+            subject: '',
             message: ''
         }
+        this.baseState = {...this.state}
     }
 
-    handleFormSubmit = (event) => {
-        event.preventDefault();
-        console.log(this.state);
+    handleSubmit = e => {
+        e.preventDefault();
+        axios({
+            method: "POST",
+            url: "https://localhost:3002/send",
+            data: {
+                name: this.state.name,
+                email: this.state.email,
+                subject: this.state.subject,
+                message: this.state.message
+            }
+        }).then((response) => {
+            if(response.data.msg === 'success'){
+                alert("Message Sent!");
+                this.resetForm();
+            }else if(response.data.msg === 'fail'){
+                alert("Message Failed to Send!");
+            }
+        })
+    }
+
+    resetForm = () => {
+        this.setState(this.baseState);
     }
 
     render() {
         return (
             <div className={styles.contact}>
                 <h1>Contact Me</h1>
-                <p>You can reach me through the form below or through one of the social media platforms in the menu on the left.</p>
                 <hr />
                 <div className={styles.container}>
-                    <form action="#">
-                        <div className={styles.formGroup}>
-                            <label for="name">Name</label>
-                            <input type="text" id="name" name="name" placeholder="First and Last Name" value={this.state.name} onChange={e => this.setState({ name: e.target.value })}></input>
-                        </div>
-                        <div className={styles.formGroup}>
-                            <label for="email">Email</label>
-                            <input type="email" id="email" name="email" placeholder="Email Address" value={this.state.email} onChange={e => this.setState({ email: e.target.value })}></input>
-                        </div>
-                        <div className={styles.formGroup}>
-                            <label for="message">Message</label>
-                            <textarea placeholder="Enter your message here" id="message" name="message" value={this.state.message} onChange={e => this.setState({ message: e.target.value })}></textarea>
-                        </div>
-                        <input type="submit" onClick={e => this.handleFormSubmit(e)} value="Submit" />
+                    <form>
+                            <input type="text" placeholder="First and Last Name"></input>
+                            <input type="email" placeholder="Email Address"></input>
+                            <input type="text" placeholder="Subject"></input>
+                            <textarea placeholder="Enter your message here"></textarea>
+                        <button type="submit">Send Message</button>
                     </form>
                 </div>
             </div >
@@ -45,4 +59,3 @@ const Contact = class Contact extends React.Component {
 }
 
 export default Contact;
-// https://blog.bitsrc.io/how-to-build-a-contact-form-with-react-js-and-php-d5977c17fec0
