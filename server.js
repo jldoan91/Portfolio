d4986c8d2c050e0db4f6efe2593a19cf920cf9bc
Josh Doan <joshd@TagPay.com>
2019-07-17 12:04:44 -0700

    Contact form functionality and route added

--- server.js
+++ server.js
@@ -3,6 +3,11 @@ const express = require('express');
 const path = require('path');
 const port = process.env.PORT || 8080;
 const app = express();
+const bodyParser = require('body-parser');
+const nodemailer = require('nodemailer');
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json())
 
 // app.use(favicon(__dirname + '/dist/favicon.ico'));
 
@@ -14,6 +19,38 @@ app.get('/', function (req, res) {
 
 app.get('/resume', (req, res) => {
     res.sendFile(path.join(__dirname, "Josh Doan's resume.pdf"));
+})
+
+let transporter = nodemailer.createTransport({
+    service: 'Gmail',
+    auth: {
+        user: proccess.env.GMAIL__USER,
+        pass: process.env.GMAIL__PASS
+    }
+});
+
+transporter.verify((error, success) => {
+    if (error) {
+        console.log(error);
+    } else {
+        console.log('Server is ready to take messages');
+    }
+})
+
+app.post('/contact', function (req, res) {
+    let mail = {
+        from: req.body.email,
+        to: 'jldoanfreelance@gmail.com',
+        subject: req.body.name + ' - ' + req.body.subject,
+        text: req.body.message
+    }
+    transporter.sendMail(mail, (err, response) => {
+        if (err) {
+            res.json({ msg: 'fail' });
+        } else {
+            res.json({ msg: 'success' });
+        }
+    })
 })
 
 app.listen(port);
--- src/components/contact/contact.js
+++ src/components/contact/contact.js
@@ -15,31 +15,42 @@ const Contact = class Contact extends React.Component {
         }
     }
 
-    formSubmit = (e) => {
-        e.preventDefault();
-
-        this.setState({
-            btnTxt: '...sending'
-        })
-
-        let data = {
-            name: this.state.name,
-            email: this.state.email,
-            subject: this.state.subject,
-            message: this.state.message
-        }
-
-
-    }
-
     resetForm = () => {
         this.setState({
             name: '',
             email: '',
             subject: '',
             message: '',
+            sent: true,
             btnTxt: 'Message Sent'
         })
+    }
+
+    formSubmit = (e) => {
+        e.preventDefault()
+
+        this.setState({
+            btnTxt: '...sending'
+        })
+
+        axios({
+            method: "POST",
+            url: "/contact",
+            data: {
+                name: this.state.name,
+                email: this.state.email,
+                subject: this.state.subject,
+                message: this.state.message
+            }
+        }).then((response => {
+            if (response.data.msg === 'success') {
+                this.resetForm()
+            } else if (response.data.msg === 'fail') {
+                this.setState({
+                    btnTxt: 'Message Failed to Send'
+                })
+            }
+        }))
     }
 
     render() {
@@ -48,12 +59,12 @@ const Contact = class Contact extends React.Component {
                 <h1>Contact Me</h1>
                 <hr />
                 <div className={styles.container}>
-                    <form onSubmit={e => this.formSubmit(e)}>
-                        <input type="text" placeholder="First and Last Name" required onChange={e => this.setState({ name: e.target.value })}></input>
-                        <input type="email" placeholder="Email Address" onChange={e => this.setState({ email: e.target.value })}></input>
-                        <input type="text" placeholder="Subject" onChange={e => this.setState({ subject: e.target.value })}></input>
-                        <textarea placeholder="Enter your message here" required onChange={e => this.setState({ message: e.target.value })}></textarea>
-                        <button disabled={this.state.sent} type="submit">{this.state.btnTxt}</button>
+                    <form>
+                        <input type="text" placeholder="First and Last Name" name="name" value={this.state.name} required onChange={e => this.setState({ name: e.target.value })}></input>
+                        <input type="email" placeholder="Email Address" name="email" value={this.state.email} onChange={e => this.setState({ email: e.target.value })}></input>
+                        <input type="text" placeholder="Subject" name="subject" value={this.state.subject} onChange={e => this.setState({ subject: e.target.value })}></input>
+                        <textarea placeholder="Enter your message here" name="message" value={this.state.message} required onChange={e => this.setState({ message: e.target.value })}></textarea>
+                        <button onClick={e => this.formSubmit(e)} disabled={this.state.sent} type="submit">{this.state.btnTxt}</button>
                     </form>
                 </div>
             </div >
