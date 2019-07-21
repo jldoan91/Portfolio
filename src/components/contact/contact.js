8682f2152fd404878a000cf04bdeeee249a37c9e
GIT SERVICE NAME (NOT USERNAME) <GIT SERVICE EMAIL>
2019-07-20 19:21:53 -0700

    social media icons

--- src/components/contact/contact.js
+++ src/components/contact/contact.js
@@ -68,9 +68,10 @@ const Contact = class Contact extends React.Component {
                     </form>
                 </div>
                 <div className={styles.socialmedia}>
-                    <a href="https://github.com/jldoan91" target="_blank"><i className={`fab fa-github fa-5x ${styles.icon}`}></i></a>
-                    <a href="https://twitter.com/jldoan91" target="_blank"><i className={`fab fa-twitter fa-5x ${styles.icon}`}></i></a>
-                    <a href="#" target="_blank"><i className={`fab fa-linkedin fa-5x ${styles.icon}`}></i></a>
+                    <a href="https://www.linkedin.com/in/josh-doan-328995138" target="_blank"><i className={`fab fa-linkedin fa-5x ${styles.icon}`}></i></a>
+                    <a href="https://github.com/jldoan91" target="_blank"><i className={`fab fa-github-square fa-5x ${styles.icon}`}></i></a>
+                    <a href="#" target="_blank"><i className={`fab fa-facebook-square fa-5x ${styles.icon}`}></i></a>
+                    <a href="https://twitter.com/jldoan91" target="_blank"><i className={`fab fa-twitter-square fa-5x ${styles.icon}`}></i></a>
                 </div>
             </div >
         )
